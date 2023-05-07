12597faa3a162d0f0b4761c9723b0d6437744c9b
aylazai <a544khan@uwaterloo.ca>
2023-05-07 19:10:25 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,138 @@
+module.exports = {
+  siteMetadata: {
+    // Site URL for when it goes live
+    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    // Your Name
+    name: 'Ryan Fitzgerald',
+    // Main Site Title
+    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    // Description that goes under your name in main bio
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    // Optional: Twitter account handle
+    author: `@rfitzio`,
+    // Optional: Github account URL
+    github: `https://github.com/ryanfitzgerald`,
+    // Optional: LinkedIn account URL
+    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    // Content of the About Me section
+    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    // Optional: List your projects, they must have `name` and `description`. `link` is optional.
+    projects: [
+      {
+        name: 'Devfolio',
+        description:
+          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+      {
+        name: 'ChromeExtensionKit',
+        description:
+          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+        link: 'https://chromeextensionkit.com/?ref=devfolio',
+      },
+      {
+        name: 'Another Cool Project',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+    ],
+    // Optional: List your experience, they must have `name` and `description`. `link` is optional.
+    experience: [
+      {
+        name: 'Acme Corp',
+        description: 'Full-Stack Developer, February 2020 - Present',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+      {
+        name: 'Globex Corp',
+        description: 'Full-Stack Developer, December 2017 - February 2020',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+      {
+        name: 'Hooli',
+        description: 'Full-Stack Developer, May 2015 - December 2017',
+        link: 'https://github.com/RyanFitzgerald/devfolio',
+      },
+    ],
+    // Optional: List your skills, they must have `name` and `description`.
+    skills: [
+      {
+        name: 'Languages & Frameworks',
+        description:
+          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+      },
+      {
+        name: 'Databases',
+        description: 'MongoDB, PostreSQL, MySQL',
+      },
+      {
+        name: 'Other',
+        description:
+          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              wrapperStyle: `margin: 0 0 30px;`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `devfolio`,
+        short_name: `devfolio`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`, // This color appears on mobile
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
+};
